8f6ede50992518964e4739ce907b99e752f06082
jw <>
2020-07-20 09:54:27 +0900

    Object generated

--- third_project/object.js
+++ third_project/object.js
@@ -1,14 +1,24 @@
-let dealer = ["Prapor", "Therapist", "Skier", "Peacekeeper", "Mechanic", "Ragman", "Jaeger", "Fence"];
+let dealer = [
+    "Prapor",
+    "Therapist",
+    "Skier",
+    "Peacekeeper",
+    "Mechanic",
+    "Ragman",
+    "Jaeger",
+    "Fence"
+];
+
 let questRequirementMap = {
     "Prapor": {
         "Ice cream cones": {
             "60-round mags for AK": 6
         },
         "The Punisher - Part 2": {
-            "lower half-mask": 7
+            "Lower half-mask": 7
         },
         "The Punisher - Part 4": {
-            "knives Bars A-2607": 5
+            "Knives Bars A-2607": 5
         },
         "The Punisher - Part 5": {
             "AK-74N": 3,
@@ -23,7 +33,7 @@ let questRequirementMap = {
             "M67 grenade": 10
         },
         "Regulated materials": {
-            "military battery": 1,
+            "Military battery": 1,
             "30-mil. shells for BMP cannon": 10
         }
     },
@@ -32,26 +42,26 @@ let questRequirementMap = {
             "Salewa kit": 5
         },
         "Sanitary Standards - Part 1": {
-            "gas analyzer": 1
+            "Gas analyzer": 1
         },
         "Sanitary Standards - Part 2": {
-            "gas analyzer": 2
+            "Gas analyzer": 2
         },
         "Painkiller": {
             "Morphine injector": 4
         },
         "General wares": {
-            "cans of meat": 15
+            "Cans of meat": 15
         },
         "Car repair": {
-            "car battery": 4,
-            "spark plug": 8
+            "Car battery": 4,
+            "Spark plug": 8
         },
         "Trust regain": {
             "303 dorm room key": 1,
             "ZB-014 key": 1,
-            "the customs military checkpoint key": 1,
-            "gas station closet key": 1
+            "The customs military checkpoint key": 1,
+            "Gas station closet key": 1
         }
     },
     "Skier": {
@@ -80,7 +90,7 @@ let questRequirementMap = {
             "Ox bleach": 2
         },
         "Spa Tour - Part 5": {
-            "the key from the locked resort rooms": 1
+            "The key from the locked resort rooms": 1
         },
         "Spa Tour - Part 7": {
             "Morphine injector": 4,
@@ -167,11 +177,11 @@ let questRequirementMap = {
             "Shturman's key": 1
         },
         "Ambulance": {
-            "portable defibrillator": 2,
+            "Portable defibrillator": 2,
             "CMS surgery kit": 2
         },
         "Shady business": {
-            "flash drive": 3
+            "Flash drive": 3
         },
         "Fishing place": {
             "TerraGroup Labs access keycard": 3
@@ -198,16 +208,17 @@ let questRequirementMap = {
         }
     }
 }
+
 let questRequirementMapInRaid = {
     "Prapor": {
         "The Punisher - Part 2": {
-            "lower half-mask": 7
+            "Lower half-mask": 7
         },
         "The Punisher - Part 4": {
-            "knives Bars A-2607": 5
+            "Knives Bars A-2607": 5
         },
         "Regulated materials": {
-            "military battery": 1,
+            "Military battery": 1,
             "30-mil. shells for BMP cannon": 10
         }
     },
@@ -219,7 +230,7 @@ let questRequirementMapInRaid = {
             "Morphine injector": 4
         },
         "General wares": {
-            "cans of meat": 15
+            "Cans of meat": 15
         },
         "Car repair": {
             "Car battery": 4,
@@ -357,6 +368,177 @@ let questRequirementMapInRaid = {
             "Shroud harf-mask": 1
         }
     },
+}
+
+let questTotalRequirement = {
+    "60-round mags for AK": 6,
+    "Lower half-mask": 7,
+    "Knives Bars A-2607": 5,
+    "AK-74N": 3,
+    "M4A1 assault rifle": 3,
+    "PM pistol": 5,
+    "PMC USEC dogtag": 14,
+    "PMC BEAR dogtag": 7,
+    "M67 grenade": 10,
+    "Military battery": 1,
+    "30-mil. shells for BMP cannon": 10,
+    "Salewa kit": 5,
+    "Gas analyzer": 3,
+    "Morphine injector": 8,
+    "Cans of meat": 15,
+    "Car battery": 4,
+    "Spark plug": 8,
+    "303 dorm room key": 1,
+    "ZB-014 key": 1,
+    "The customs military checkpoint key": 1,
+    "Gas station closet key": 1,
+    "Secure Flash drive": 2,
+    "Medical bloodset": 3,
+    "Respirator": 4,
+    "False flash drive": 0,
+    "MRE ration pack": 5,
+    "WD-40 100ml": 1,
+    "Clin wiper": 2,
+    "Corrugated hose": 4,
+    "Ox bleach": 2,
+    "The key from the locked resort rooms": 1,
+    "Heat-exchange alkali surface washer": 2,
+    "5L propane tank": 2,
+    "Military COFDM wireless Signal Transmitter": 0,
+    "Virtex programmable processor": 2,
+    "Powercord": 2,
+    "T-Shaped Plug": 4,
+    "Printed circuit board": 5,
+    "Graphics card": 3,
+    "CPU Fan": 8,
+    "PC CPU": 3,
+    "Rechargeable battery": 3,
+    "Broken GPhone": 3,
+    "Malboro cigarette": 5,
+    "Strike cigarette": 5,
+    "Wilston cigarette": 5,
+    "UHF RFID Reader": 1,
+    "VPX Flash Storage Module": 1,
+    "Wires": 5,
+    "Capacitors": 5,
+    "Ski hat with holes for eyes": 1,
+    "Pilgrim tourist backpack": 1,
+    "Wartech (TV-109, TV-106)": 2,
+    "BlackRock chest rig": 2,
+    "Fuel conditioner": 4,
+    "Bronze lion": 2,
+    "Horse figurine": 2,
+    "Cat figurine": 1,
+    "Roler submariner gold wrist watch": 1,
+    "Antique teapot": 3,
+    "Antique vase": 2,
+    "Aramid fabric": 5,
+    "Ripstop fabric": 10,
+    "Paracord": 3,
+    "Fleece fabric": 5,
+    "Polyaramide cordura fabric": 10,
+    "KEKtape": 5,
+    "Golden TT": 1,
+    "Killa's helmet": 1,
+    "Shturman's key": 1,
+    "Portable defibrillator": 2,
+    "CMS surgery kit": 2,
+    "Flash drive": 3,
+    "TerraGroup Labs access keycard": 3,
+    "Old firesteel": 1,
+    "Antique axe": 1,
+    "Battered antique Book": 1,
+    "FireKlean gun lube": 1,
+    "Golden rooster": 1,
+    "Silver Badge": 1,
+    "Deadlyslob's beard oil": 1,
+    "Golden 1GPhone": 1,
+    "Jar of DevilDog mayo": 1,
+    "Can of sprats": 1,
+    "Fake mustache": 1,
+    "Kotton beanie": 1,
+    "Can of Dr. Lupo's coffee beans": 1,
+    "Pestily plague mask": 1,
+    "Raven figurine": 1,
+    "Shroud half-mask": 1
+}
+
+let questTotalRequirementInRaid = {
+    "Lower half-mask": 7,
+    "Knives Bars A-2607": 5,
+    "Military battery": 1,
+    "30-mil. shells for BMP cannon": 10,
+    "Gas analyzer": 2,
+    "Morphine injector": 8,
+    "Cans of meat": 15,
+    "Car battery": 4,
+    "Spark plug": 8,
+    "Secure Flash drive": 2,
+    "Medical bloodset": 3,
+    "Respirator": 4,
+    "WD-40 100ml": 1,
+    "Clin wiper": 2,
+    "Corrugated hose": 4,
+    "Ox bleach": 2,
+    "Heat-exchange alkali surface washer": 2,
+    "5L propane tank": 2,
+    "Military COFDM wireless Signal Transmitter": 0,
+    "Virtex programmable processor": 2,
+    "Powercord": 2,
+    "T-Shaped Plug": 4,
+    "Printed circuit board": 5,
+    "Graphics card": 3,
+    "CPU Fan": 8,
+    "PC CPU": 3,
+    "Rechargeable battery": 3,
+    "Broken GPhone": 3,
+    "Malboro cigarette": 5,
+    "Strike cigarette": 5,
+    "Wilston cigarette": 5,
+    "UHF RFID Reader": 1,
+    "VPX Flash Storage Module": 1,
+    "Wires": 5,
+    "Capacitors": 5,
+    "Ski hat with holes for eyes": 1,
+    "Pilgrim tourist backpack": 1,
+    "Wartech (TV-109, TV-106)": 2,
+    "BlackRock chest rig": 2,
+    "Fuel conditioner": 4,
+    "Bronze lion": 2,
+    "Horse figurine": 2,
+    "Cat figurine": 1,
+    "Roler submariner gold wrist watch": 1,
+    "Antique teapot": 3,
+    "Antique vase": 2,
+    "Aramid fabric": 5,
+    "Ripstop fabric": 10,
+    "Paracord": 3,
+    "Fleece fabric": 5,
+    "Polyaramide cordura fabric": 10,
+    "KEKtape": 5,
+    "Golden TT": 1,
+    "Killa's helmet": 1,
+    "Shturman's key": 1,
+    "Portable defibrillator": 2,
+    "CMS surgery kit": 2,
+    "Flash drive": 3,
+    "TerraGroup Labs access keycard": 3,
+    "Old firesteel": 1,
+    "Antique axe": 1,
+    "Battered antique Book": 1,
+    "FireKlean gun lube": 1,
+    "Golden rooster": 1,
+    "Silver Badge": 1,
+    "Deadlyslob's beard oil": 1,
+    "Golden 1GPhone": 1,
+    "Jar of DevilDog mayo": 1,
+    "Can of sprats": 1,
+    "Fake mustache": 1,
+    "Kotton beanie": 1,
+    "Can of Dr. Lupo's coffee beans": 1,
+    "Pestily plague mask": 1,
+    "Raven figurine": 1,
+    "Shroud harf-mask": 1
 }
 
 let questCompletetMap = {
