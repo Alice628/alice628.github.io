75e6e7ae02c28e8b114fdab26260ed331c0294ee
Alice628 <39880235+Alice628@users.noreply.github.com>
2021-06-21 16:31:03 +0900

    1차 완료

---
+++ accident_calendar/config.js
@@ -0,0 +1,14 @@
+let eventConfic = {
+    eventArr: [
+        {       /*사고일 부터*/
+            days: 7/*일 동안*/,
+            times: 7/*번 방문 할 수 있다.*/,
+            repetition: 3/*번의 기간 반복 */,
+        },
+        {       /*위 기간 이후에*/
+            days: 7/*일 동안*/,
+            times: 3/*번 방문 할 수 있다.*/,
+            repetition: 1/*번의 기간 반복 */,
+        },
+    ],
+};
